acb9eb81ede94d5d854e38004585504b0fc68978
nikamamaladze92 <nikamamaladze92@gmail.com>
2026-05-17 14:39:43 -0400

    ma

--- apps/web/src/pages/TicketsPage.jsx
+++ apps/web/src/pages/TicketsPage.jsx
@@ -3,6 +3,24 @@ import client from "../api/client";
 
 const priorityOptions = ["High", "Medium", "Low"];
 
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+
 function formatDepartment(value) {
   switch (value) {
     case "warehouse":
