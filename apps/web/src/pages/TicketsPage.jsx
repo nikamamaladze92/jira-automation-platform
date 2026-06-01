d2d7c44b29540d9a79471306057c8b648d7f26b9
nikamamaladze92 <nikamamaladze92@gmail.com>
2026-05-31 20:20:53 -0400

    ffff

--- apps/web/src/pages/TicketsPage.jsx
+++ apps/web/src/pages/TicketsPage.jsx
@@ -3,6 +3,58 @@ import client from "../api/client";
 
 const priorityOptions = ["High", "Medium", "Low"];
 
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+//delete later
+
 function formatDepartment(value) {
   switch (value) {
     case "warehouse":
