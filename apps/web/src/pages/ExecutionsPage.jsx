7ebf75ea7cac258b455fba2b3d32157fbd35ec03
nikamamaladze92 <nikamamaladze92@gmail.com>
2026-06-29 14:47:17 -0400

    ssssd

--- apps/web/src/pages/ExecutionsPage.jsx
+++ apps/web/src/pages/ExecutionsPage.jsx
@@ -467,18 +467,6 @@
 //   fontWeight: 800,
 // };
 
-//delete later
-
-//delete later
-//delete later
-
-//delete later
-//delete later
-//delete later
-//delete later
-//delete later
-//delete later
-
 import { useEffect, useState, useMemo } from "react";
 import client from "../api/client";
 
